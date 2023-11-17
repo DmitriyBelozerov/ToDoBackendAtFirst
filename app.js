ca376cf85f7aaeb88ace95a6c4e6b13ef2d2e996
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-11-17 23:10:56 +0300

    feat: mongodb

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
-// Слушаем 3000 порт
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/mytododb');
+
+app.use(bodyParser.json())
 
 app.use('/', router);
 
